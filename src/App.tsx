fe16dd5aeb9e5da26724094667f0fae523f368e0
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-03-11 15:21:26 -0300

    feat: list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Button from './components/button';
 import Forms from './components/forms';
+import List from './components/list';
 
 function App() {
   return (
     <div className="App">
       <Forms/>
+      <List/>
     </div>
   )
 }
