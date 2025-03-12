f7ba0db956cd86248654ca9bb3fa532d8fed9ec5
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-03-12 08:52:22 -0300

    add timer component to app page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Forms from '../components/forms';
 import List from '../components/list';
 import style from './App.module.scss'
+import Timer from '../components/timer';
 
 function App() {
   return (
     <div className={style.AppStyle}>
       <Forms/>
       <List/>
+      <Timer/>
     </div>
   )
 }
