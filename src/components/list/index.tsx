a7727b6d6d6be8a1494b7531d9cfc5dde7b03eb0
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-03-12 19:17:29 -0300

    feat: usestate

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './list.module.scss';
 import Item from './item';
 
 function List() {
-    const tasks = [{
+    const [tasks, setTasks] = useState([{
         task: 'React',
         time: '02:00:00'
     },
@@ -14,10 +14,12 @@ function List() {
     {
         task: 'typescript',
         time: '10:00:00'
-    }]
+    }]);
     return(
         <aside className={style.taskList}>
-            <h2> daily studies</h2>
+            <h2 onClick={() => {
+                setTasks([...tasks, { task: "study states", time: "05:00:00"}])
+            }}> daily studies</h2>
             <ul>
                 {tasks.map((item, index) => (
                     <Item
