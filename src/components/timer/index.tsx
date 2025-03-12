3dfcd9968d58d9501b278621044c76c32551ebf2
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-03-12 19:06:43 -0300

    feat: timer css

--- src/components/timer/index.tsx
+++ src/components/timer/index.tsx
@@ -1,15 +1,16 @@
 import Button from "../button";
 import Watch from "./watch";
+import style from './timer.module.scss'
 
 export default function Timer() {
     return (
-        <div>
-            <p>Choose your card and start the timer.</p>
-            <div>
+        <div className={style.timer}>
+            <p className={style.title}>Choose your card and start the timer.</p>
+            <div className={style.watchWrapper}>
                 <Watch />
             </div>
             <Button 
-                button="Start"
+                button="start"
             />
         </div>
     )
