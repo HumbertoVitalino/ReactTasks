5e02504019adbd83786923d2f7e0bead265b7cab
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-03-13 09:01:38 -0300

    refactor: timer

--- src/components/timer/index.tsx
+++ src/components/timer/index.tsx
@@ -1,11 +1,23 @@
 import Button from "../button";
 import Watch from "./watch";
 import style from './timer.module.scss'
+import { ITask } from "../../types/task";
+import { useState } from "react";
+import { timeForSeconds } from "../../common/utils/time";
 
-export default function Timer() {
+interface Props {
+    selected: ITask | undefined
+}
+
+export default function Timer({ selected }: Props) {
+    const [time, setTime] = useState<number>();
+    if(selected?.time) {
+        setTime(timeForSeconds(selected.time))
+    }
     return (
         <div className={style.timer}>
             <p className={style.title}>Choose your card and start the timer.</p>
+            Time: {time}
             <div className={style.watchWrapper}>
                 <Watch />
             </div>
