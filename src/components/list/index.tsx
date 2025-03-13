70e46d16b1a865f99727e529717cae0351fd4ca7
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-03-13 08:12:47 -0300

    refactor: list

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,25 +1,13 @@
 import React, { useState } from 'react';
 import style from './list.module.scss';
 import Item from './item';
+import { ITask } from '../../types/task';
 
-function List() {
-    const [tasks, setTasks] = useState([{
-        task: 'React',
-        time: '02:00:00'
-    },
-    {
-        task: 'javascript',
-        time: '01:00:00'
-    },
-    {
-        task: 'typescript',
-        time: '10:00:00'
-    }]);
+function List({ tasks }: {tasks: ITask[]} ) {
+    
     return(
         <aside className={style.taskList}>
-            <h2 onClick={() => {
-                setTasks([...tasks, { task: "study states", time: "05:00:00"}])
-            }}> daily studies</h2>
+            <h2> daily studies</h2>
             <ul>
                 {tasks.map((item, index) => (
                     <Item
