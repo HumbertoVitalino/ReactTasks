9f71222ca554e4c54e32228bc16ab73fddc00608
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-03-13 08:13:16 -0300

    refactor: app to usestate

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Forms from '../components/forms';
 import List from '../components/list';
 import style from './App.module.scss'
 import Timer from '../components/timer';
+import { ITask } from '../types/task';
 
 function App() {
+  const [tasks, setTasks] = useState<ITask[] | []>([]);
   return (
     <div className={style.AppStyle}>
-      <Forms/>
-      <List/>
+      <Forms setTasks={setTasks}/>
+      <List tasks={tasks}/>
       <Timer/>
     </div>
   )
