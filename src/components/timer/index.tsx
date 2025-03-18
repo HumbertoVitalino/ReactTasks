7fae1808be50684e322ce1f70ca4f4ffd5e51d1f
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-03-17 22:13:40 -0300

    refactor: timer

--- src/components/timer/index.tsx
+++ src/components/timer/index.tsx
@@ -2,7 +2,7 @@ import Button from "../button";
 import Watch from "./watch";
 import style from './timer.module.scss'
 import { ITask } from "../../types/task";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { timeForSeconds } from "../../common/utils/time";
 
 interface Props {
@@ -11,19 +11,22 @@ interface Props {
 
 export default function Timer({ selected }: Props) {
     const [time, setTime] = useState<number>();
-    if(selected?.time) {
-        setTime(timeForSeconds(selected.time))
-    }
+
+    useEffect(() => {
+        if(selected?.time) {
+            setTime(timeForSeconds(selected.time))
+        }
+    },[selected])
     return (
         <div className={style.timer}>
             <p className={style.title}>Choose your card and start the timer.</p>
             Time: {time}
             <div className={style.watchWrapper}>
-                <Watch />
+                <Watch time={time}/>
             </div>
-            <Button 
-                button="start"
-            />
+            <Button>
+                button='start'
+            </Button>
         </div>
     )
 }
--- src/components/timer/watch/index.tsx
+++ src/components/timer/watch/index.tsx
@@ -1,13 +1,21 @@
 import style from './watch.module.scss'
 
-export default function Watch() {
+interface Props {
+    time: number | undefined
+}
+
+export default function Watch({ time = 0 }: Props) {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    const [minutesDez, minutesUni] = String(minutes).padStart(2, '0');
+    const [secondDez, secondUni] = String(seconds).padStart(2, '0');
     return (
         <>
-            <span className={style.watchNumber}>0</span>
-            <span className={style.watchNumber}>0</span>
+            <span className={style.watchNumber}>{minutesDez}</span>
+            <span className={style.watchNumber}>{minutesUni}</span>
             <span className={style.watchDiv}>:</span>
-            <span className={style.watchNumber}>0</span>
-            <span className={style.watchNumber}>0</span>
+            <span className={style.watchNumber}>{secondDez}</span>
+            <span className={style.watchNumber}>{secondUni}</span>
         </>
     )
 }
